1ce6bf196dc7c0bc4bf363a02760b28bc353360b
Jared Rainey <jared.rainey.it@gmail.com>
2021-11-23 10:59:45 -0500

    initial commit

---
+++ src/components/app.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {render, screen, cleanup} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import App from './app';
+
+afterEach(() => cleanup);
+
+describe("<App/>", () => {
+    it('renders', async () => {
+        render(<App audience="Jared"/>);
+    });
+
+    it("has correct audience", () => {
+        const audience = 'Jared';
+        const {findByText} = render(<App audience="Jared"/>);
+        findByText(new RegExp('Hello, '+ audience, 'i'));
+    })
+});
---
+++ src/components/app.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = (props:any) => <h1>Hello, {props.audience}!</h1>;
+
+export default App;
